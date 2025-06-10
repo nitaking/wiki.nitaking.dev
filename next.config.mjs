b2b1b3991ee13d123da9c4dfe3efed33bcdd10bd
nitaking <10850034+nitaking@users.noreply.github.com>
2025-06-10 09:23:03 +0900

    fix: error

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  remotePatterns: [new URL('https://raw.githubusercontent.com/nitaking/media/main/wiki.nitaking.dev/**')],
 };
 
 export default withMDX(config);
