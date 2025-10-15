4b5190bb0ec3fecc14b43bcb3876be516dfa0917
nitaking <10850034+nitaking@users.noreply.github.com>
2025-10-15 18:21:55 +0900

    chore: add tinylytics analytics embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
-import {Metadata} from "next";
+import { Metadata } from 'next';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -20,6 +21,10 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
+        <Script
+          src="https://tinylytics.app/embed/U9qfcSxsEP_izZvF-UUN.js"
+          strategy="afterInteractive"
+        />
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
